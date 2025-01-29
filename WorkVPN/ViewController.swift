9a46af8c43b11e6cf733e165001de6a9661093f6
Александр Муклинов <dkan425@gmail.com>
2025-01-29 11:34:06 +0700

    Initial Commit

---
+++ WorkVPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkVPN
+//
+//  Created by Александр Муклинов on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
